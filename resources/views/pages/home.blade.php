5525b18afdb7247c564981c6acd0a0664a4b27c4
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-02-08 16:05:28 +0100

    Creo layout pagina home

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.main-layout')
+
+
+@include('content.header')
+@section('content')
+
+<div class="container">
+    <h1 class="text-center mt-3">Portfolio di Fabio babaglioni</h1>
+
+    <div class="ms_card">
+        <h3>Titolo Azienda</h3>
+        <div>Programmi utilizzati</div>
+        <div>link</div>
+    </div>
+</div>
+
+
+@endsection
+
+@include('content.footer')
